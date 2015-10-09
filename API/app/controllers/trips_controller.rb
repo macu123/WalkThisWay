889920aca34d9065c1e38431212fe07833625cfc
samkessaram <samkessaram@gmail.com>
2015-10-09 19:48:00 +0000

    started google api call

--- API/app/controllers/trips_controller.rb
+++ API/app/controllers/trips_controller.rb
@@ -1,2 +1,18 @@
 class TripsController < ApplicationController
+
+  def create
+
+    url = 'https://maps.googleapis.com/maps/api/directions/json?'
+    origin = ""
+    destination = ""
+
+    key = "AIzaSyCR5fUOPVxtqsSR5Oy3jIQ4P-f0tLMYj9k"
+
+    mode = "walking"
+
+    response = HTTParty.get('#{url}origin=#{origin}&destination=#{destination}&key=#{key}')
+  end
+
 end
+
+
