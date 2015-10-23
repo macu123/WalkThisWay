2f03d9ccc3a99b6fa02a5416934d88249aa0b083
samkessaram <samkessaram@gmail.com>
2015-10-23 03:32:10 +0000

    asdf

--- app/services/trip_planner.rb
+++ app/services/trip_planner.rb
@@ -88,7 +88,7 @@ class TripPlanner
     elsif /[A-Z]/.match(@route_tag)
       @error = "Your trip is beyond the realm of the TTC (or this app...). Godspeed."
     elsif @short_trip
-      @error = "This trip is too short for us to calculate a transit route for. In other words, just walk."
+      @error = "This trip is too short for us to calculate a transit route. In other words, just walk."
     else
       @error = false
     end
